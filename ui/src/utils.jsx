99e1107b59f35963234ca8a304cc3ce72df80aaa
prayag719 <2153003@ritindia.edu>
2023-11-26 16:53:37 +0530

    feat: updated api url

--- ui/src/utils.jsx
+++ ui/src/utils.jsx
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://xlegv3aprnnpvadb2wdu2d3e740lwtyn.lambda-url.us-west-1.on.aws/task";
+  "https://opp4nyzkdxp4lmo52yymppafku0gcuat.lambda-url.us-west-1.on.aws/task";
